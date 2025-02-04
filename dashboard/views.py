130ff4e744318afd4e3dcbafebdf0358bd9f5b14
AshLaw96 <ash.l.96@outlook.com>
2025-02-04 20:06:48 +0000

    Add to dashboard app to link correct sections to each page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from listings.models import Caravan
+from django.utils import timezone
+from listings.models import Caravan, Booking
 
 
 @login_required
@@ -32,12 +33,22 @@ def dashboard_view(request):
     # Check user type and populate context
     if user_profile.user_type == 'customer':
         context['user_type'] = 'customer'
-        context['upcoming_stays'] = []
+        context['upcoming_stays'] = Booking.objects.filter(
+            customer=current_user,
+            status='accepted',
+            start_date__gte=timezone.now()
+        )
         context['wishlist'] = []
-        context['previous_stays'] = []
+        context['previous_stays'] = Booking.objects.filter(
+            customer=current_user,
+            status='accepted',
+            end_date__lt=timezone.now()
+        )
     elif user_profile.user_type == 'owner':
         context['user_type'] = 'owner'
-        context['requests'] = []
+        context['requests'] = Booking.objects.filter(
+            caravan__owner=current_user, status='pending'
+        )
         context['caravans'] = Caravan.objects.filter(owner=current_user)
         context['reviews'] = []
     else:
