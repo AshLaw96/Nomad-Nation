36bfc99106415ac44ceedc0d908bd2fb6ddb1c65
AshLaw96 <ash.l.96@outlook.com>
2025-02-17 21:41:34 +0000

    Add views & modals to edit sections on account settings pg

--- user_settings/urls.py
+++ user_settings/urls.py
@@ -4,4 +4,20 @@ from . import views
 
 urlpatterns = [
     path('account_settings/', views.account_settings, name='account_settings'),
+    path(
+        'edit_personal_details/',
+        views.edit_personal_details,
+        name='edit_personal_details'
+    ),
+    path('edit_preferences/', views.edit_preferences, name='edit_preferences'),
+    path(
+        'edit_payment_details/',
+        views.edit_payment_details,
+        name='edit_payment_details'
+    ),
+    path(
+        'edit_privacy_settings/',
+        views.edit_privacy_settings,
+        name='edit_privacy_settings'
+    ),
 ]
--- user_settings/views.py
+++ user_settings/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.conf import settings
+from django.utils import translation
+from django.http import HttpResponseRedirect
 from .models import UserProfile, PaymentDetails, PrivacySettings
 
 
@@ -21,6 +25,34 @@ def account_settings(request):
         'privacy_settings': privacy_settings
     }
     return render(request, 'user_settings/account_settings.html', context)
+
+
+@login_required
+def edit_personal_details(request):
+    if request.method == 'POST':
+        user = request.user
+        user.username = request.POST['username']
+        user.email = request.POST['email']
+        user.save()
+
+        user_profile = UserProfile.objects.get(user=user)
+        user_profile.phone_number = request.POST['phone_number']
+        user_profile.save()
+
+        messages.success(request, 'Personal details updated successfully.')
+        return redirect('account_settings')
+
+
+@login_required
+def edit_preferences(request):
+    if request.method == 'POST':
+        user_profile = UserProfile.objects.get(user=request.user)
+        user_profile.language = request.POST['language']
+        user_profile.currency = request.POST['currency']
+        user_profile.appearance = request.POST['appearance']
+        user_profile.notifications = 'notifications' in request.POST
+        user_profile.save()
+
         # Activate the selected language
         translation.activate(user_profile.language)
         request.session[settings.LANGUAGE_COOKIE_NAME] = user_profile.language
@@ -31,3 +63,31 @@ def account_settings(request):
             settings.LANGUAGE_COOKIE_NAME, user_profile.language
         )
         return response
+
+
+@login_required
+def edit_payment_details(request):
+    if request.method == 'POST':
+        payment_details, created = PaymentDetails.objects.get_or_create(
+            user=request.user)
+        payment_details.payment_method = request.POST['payment_method']
+        payment_details.card_last_four = request.POST['card_last_four']
+        payment_details.billing_address = request.POST['billing_address']
+        payment_details.save()
+
+        messages.success(request, 'Payment details updated successfully.')
+        return redirect('account_settings')
+
+
+@login_required
+def edit_privacy_settings(request):
+    if request.method == 'POST':
+        privacy_settings, created = PrivacySettings.objects.get_or_create(
+            user=request.user)
+        privacy_settings.data_sharing_enabled = (
+            'data_sharing_enabled' in request.POST
+        )
+        privacy_settings.save()
+
+        messages.success(request, 'Privacy settings updated successfully.')
+        return redirect('account_settings')
