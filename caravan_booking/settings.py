578008adf998e4ceaa4f0e28ee46bcfaed499c56
AshLaw96 <ash.l.96@outlook.com>
2025-03-01 19:11:34 +0000

    Fix heroku issue by enabling humanize in django

--- caravan_booking/settings.py
+++ caravan_booking/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_extensions',
+    'django.contrib.humanize',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
